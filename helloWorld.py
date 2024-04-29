36660a695a853661a1bd9ecb27ba0a0f6553a27d
Samueel Abreu <samueellabreu@gmail.com>
2024-04-29 00:09:38 -0300

    Hello World

--- helloWorld.py
+++ helloWorld.py
@@ -1,4 +1,6 @@
 #  Autor: Samuel Moreira Abreu
 #  O intuito desse código é revisar os conceitos iniciais de Python para o começo em uma iniciação cientifica.
 
+# Hello World
+
 print("Hello World!")
