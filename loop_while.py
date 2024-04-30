556a35e8350eac5ba99b8f62d6f0993b372b0afb
Samueel Abreu <samueellabreu@gmail.com>
2024-04-30 00:53:04 -0300

    Loop while

---
+++ loop_while.py
@@ -0,0 +1,39 @@
+#  Autor: Samuel Moreira Abreu
+#  O intuito desse código é revisar os conceitos iniciais de Python para o começo em uma iniciação cientifica.
+
+"""
+Loop While
+
+forma geral
+
+while expressao:
+     // execucao do programa
+
+
+enquanto "expressao" for TRUE, o programa ficara em loop!
+
+exemplo
+
+numero = 0
+
+while numero < 10:
+     print(num)
+     numero += 1
+
+"""
+
+numero = 0
+
+while numero < 10:
+     print(numero)
+     numero += 1 #importante para o loop não rodar infinito!!!
+
+
+valor = ''
+
+while valor != 'sim':
+     valor = input("Já acabou Jessica? ")
+
+print("Loop acabou")
+
+
